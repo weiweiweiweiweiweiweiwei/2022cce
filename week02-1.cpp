9039e7aa9ed3d7e3714508b1b756f46bceb4d626
weiweiweiweiweiweiweiwei <112918704+weiweiweiweiweiweiweiwei@users.noreply.github.com>
2022-09-13 12:31:09 +0800

    week02

---
+++ week02-1.cpp
@@ -0,0 +1,10 @@
+///File-New-Empty 空白檔案
+///File-Save fo;e As 存成 week02-1.cpp
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello World\n");
+    printf("H\tello World\n");
+    printf("\tHello World\n");
+}
---
+++ week02-2.cpp
@@ -0,0 +1,10 @@
+///File-New-Empty 空白檔案
+///File-Save fo;e As 存成 week02-1.cpp
+#include <stdio.h>
+
+int main()
+{
+    printf("0511\n");
+    printf("這裡有字,有插入值\n");
+    printf("這裡有字%d有插入值\n",2004);
+}
